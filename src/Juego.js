a1d83fcda27b66861f8e2ce9ebaeb4d19d1db93d
2D-ONIX <tomiterran@gmail.com>
2023-06-28 18:06:46 -0300

    circulo malo

--- src/Juego.js
+++ src/Juego.js
@@ -5,10 +5,14 @@ var gameOver = false;
 var cursors;
 var triangle;
 var circle;
+var circlemalo;
 var square;
 var timedEvent;
 var n;
-
+var scoreText;
+var timeLeft = 80;
+var timeText;
+var winText;
 
 var config = {
     type: Phaser.AUTO,
@@ -47,6 +51,7 @@ function preload(){
 function create (){
     gameOver = false;
     score = 0;
+    timeLeft = 80;
 
     timedEvent = this.time.addEvent({ 
         delay: 3000, 
@@ -57,10 +62,10 @@ function create (){
 
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'cielo').setScale(2);
     
-
     platforms = this.physics.add.staticGroup();
 
     circle = this.physics.add.group();
+    circlemalo = this.physics.add.group();
     triangle = this.physics.add.group();
     square = this.physics.add.group();
 
@@ -72,24 +77,30 @@ function create (){
     
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(player, circle);
-    this.physics.add.collider(platforms, circle);
+    this.physics.add.collider(player, circlemalo);
 
     cursors = this.input.keyboard.createCursorKeys();
 
     this.physics.add.overlap(player, circle, collectCircle, null, this);
-    this.physics.add.overlap(platforms, circle, setGameOver, null, this);
+    this.physics.add.overlap(player, circlemalo, collectCirclemalo, null, this);
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    timeText = this.add.text(16, 50, 'Time left: 80', { fontSize: '32px', fill: '#000' });
+    winText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, '', { fontSize: '64px', fill: '#00ff00' });
+    winText.setOrigin(0.5);
     
 }
+
 function update(){
     if (gameOver) {
         return;
     }
 
     if (cursors.left.isDown) {
-        player.setVelocityX(-200);
+        player.setVelocityX(-400);
     
     } else if (cursors.right.isDown) {
-        player.setVelocityX(200);
+        player.setVelocityX(400);
     
     } else {
         player.setVelocityX(0);
@@ -102,17 +113,52 @@ function update(){
 function onSecond(){
     if(!gameOver){
         n = (Math.random() * (20 - 680)) + 680;
-        console.log (n);
+        console.log(n);
 
         const papa = this.add.circle(n, 50, 40, 0xff6699);
         this.physics.add.existing(papa);
         papa.body.setCircle(40);
-        circle.add(papa);  
+        circle.add(papa);
+    }
+
+    const malo = this.add.circle(n, 50, 40, 0xff0000);
+    this.physics.add.existing(malo);
+    malo.body.setCircle(40);
+    circlemalo.add(malo);
+
+    timeLeft -= 1;
+    timeText.setText('Time left: ' + timeLeft);
+
+    if (timeLeft <= 0) {
+        setGameOver();
     }
 }
+
 function collectCircle(player, circle){
     circle.destroy();
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (score >= 100) {
+        winText.setText('You Win!');
+        gameOver = true;
+    }
 }
+
+function collectCirclemalo(player, circlemalo){
+    circlemalo.destroy();
+    score -= 10;
+    scoreText.setText('Score: ' + score);
+
+    if (score <= -100) {
+        winText.setText('You Lose!');
+        gameOver = true;
+    }
+}
+
 function setGameOver(){
     gameOver = true;
+    winText.setText('Game Over');
 }
+
+
