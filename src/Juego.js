d5680ebcc01dede0d53aeddfd60d0bead60ac5a0
2D-ONIX <tomiterran@gmail.com>
2023-06-28 18:55:35 -0300

    cosas

--- src/Juego.js
+++ src/Juego.js
@@ -7,6 +7,7 @@ var triangle;
 var circle;
 var circlemalo;
 var square;
+var ROJO;
 var timedEvent;
 var n;
 var scoreText;
@@ -37,90 +38,101 @@ var config = {
             debug: true
         }
     },
-    scene:{preload: preload,create: create,update: update}
+    scene: { preload: preload, create: create, update: update }
 };
 
 var game = new Phaser.Game(config);
 
-function preload(){
-    this.load.image('cielo', 'public/assets/images/Cielo.webp')
-    this.load.image('ninja', 'public/assets/images/Ninja.png')
-    this.load.image('ground', 'public/assets/images/platform.png')
+function preload() {
+    this.load.image('cielo', 'public/assets/images/Cielo.webp');
+    this.load.image('ninja', 'public/assets/images/Ninja.png');
+    this.load.image('ground', 'public/assets/images/platform.png');
+    this.load.image('ROJO', 'public/assets/images/ROJO.png');
 }
 
-function create (){
+function create() {
     gameOver = false;
     score = 0;
     timeLeft = 80;
 
-    timedEvent = this.time.addEvent({ 
-        delay: 3000, 
-        callback: onSecond, 
-        callbackScope: this, 
-        loop: true 
+    timedEvent = this.time.addEvent({
+        delay: 3000,
+        callback: onSecond,
+        callbackScope: this,
+        loop: true
     });
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'cielo').setScale(2);
-    
+
     platforms = this.physics.add.staticGroup();
 
     circle = this.physics.add.group();
     triangle = this.physics.add.group();
     square = this.physics.add.group();
+    ROJO = this.physics.add.group();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     player = this.physics.add.sprite(100, 450, "ninja").setScale(0.15);
-    
+
     player.setCollideWorldBounds(true);
-    
+
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(player, circle);
     this.physics.add.collider(platforms, circle);
-    
-    
+    this.physics.add.collider(player, ROJO);
+    this.physics.add.collider(platforms, ROJO);
 
     cursors = this.input.keyboard.createCursorKeys();
 
     this.physics.add.overlap(player, circle, collectCircle, null, this);
+    this.physics.add.overlap(player, ROJO, hitRojo, null, this);
     this.physics.add.overlap(player, circle, collectCircle, null, this);
     this.physics.add.collider(platforms, circle, removeCircle, null, this);
-    
+    this.physics.add.collider(platforms, ROJO, removeRojo, null, this);
+
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
     timeText = this.add.text(16, 50, 'Time left: 80', { fontSize: '32px', fill: '#000' });
     winText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, '', { fontSize: '64px', fill: '#00ff00' });
     winText.setOrigin(0.5);
-    
 }
 
-function update(){
+function update() {
     if (gameOver) {
         return;
     }
 
     if (cursors.left.isDown) {
         player.setVelocityX(-400);
-    
+
     } else if (cursors.right.isDown) {
         player.setVelocityX(400);
-    
+
     } else {
         player.setVelocityX(0);
     }
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
+
+    this.physics.world.collide(ROJO, platforms, removeRojo, null, this);
 }
 
 function onSecond() {
     if (!gameOver) {
-        n = Math.random() * (20 - 680) + 680;
-        console.log(n);
+        var circleX = getRandomX();
+        var rojoX = getRandomX();
+        while (Math.abs(circleX - rojoX) < 100) {
+            rojoX = getRandomX();
+        }
 
-        const papa = this.add.circle(n, 50, 40, 0xff6699);
-        this.physics.add.existing(papa);
-        papa.body.setCircle(40);
-        circle.add(papa);
+        const circleObj = this.add.circle(circleX, 50, 40, 0xff6699);
+        this.physics.add.existing(circleObj);
+        circleObj.body.setCircle(40);
+        circle.add(circleObj);
 
+        const rojoObj = this.physics.add.sprite(rojoX, 50, 'ROJO');
+        rojoObj.setVelocityY(100);
+        ROJO.add(rojoObj);
     }
 
     timeLeft -= 1;
@@ -131,13 +143,15 @@ function onSecond() {
     }
 }
 
-
-function removeCircle(platform, circle) {
+function removeCircle(platforms, circle) {
     circle.destroy();
 }
 
+function removeRojo(platforms, rojo) {
+    rojo.destroy();
+}
 
-function collectCircle(player, circle){
+function collectCircle(player, circle) {
     circle.destroy();
     score += 10;
     scoreText.setText('Score: ' + score);
@@ -148,7 +162,20 @@ function collectCircle(player, circle){
     }
 }
 
-function setGameOver(){
+function hitRojo(player, rojo) {
+    rojo.destroy();
+    score -= 10;
+    scoreText.setText('Score: ' + score);
+}
+
+function setGameOver() {
     gameOver = true;
     winText.setText('Game Over');
 }
+
+function getRandomX() {
+    return Math.random() * (800 - 80) + 80;
+}
+
+
+
